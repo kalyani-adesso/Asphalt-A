49568cdaa095213ed2ac35ba8c207843a30af2ed
kalyani.ausekar <kalyani.ausekar@adesso.in>
2025-09-25 10:55:26 +0530

    font colors

--- commonui/src/main/java/com/asphalt/commonui/theme/Color.kt
+++ commonui/src/main/java/com/asphalt/commonui/theme/Color.kt
@@ -3,19 +3,10 @@ package com.asphalt.commonui.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryDarkerLightB75 = Color(0xFF006EC7)
-val PrimaryDarkerLightB25 = Color(0xFF025FBF)
-val PrimaryDarkerLightB50 = Color(0xFF013F80)
-val PrimaryDarker38 = Color(0xFF001E38)
-
+val PrimaryDarkerLightB50 = Color(0xFF132F4F)
 val PrimaryLight = Color(0xFF027EFF)
-val PrimaryBrighterLightW75 = Color(0xFFC0DFFF)
-val PrimaryBrigtherLightW50 = Color(0xFF80BEFF)
+val PrimaryBrighterLightW75 = Color(0xFF132F4F)
 val PrimaryBrighterLightW25 = Color(0xFF419EFF)
-
-val SecondarySuccess = Color(0xFF0AC875)
-val SecondaryWarning = Color(0xFFFE6836)
-val SecondaryCritical = Color(0xFFD40828)
-val SecondaryCriticalLight = Color(0x33D40828)
 
 val NeutralWhite = Color(0xFFFFFFFF)
 val NeutralPaperGrey = Color(0xFFF5F5F5)
@@ -25,8 +16,3 @@ val NeutralGrey = Color(0xFF999999)
 val NeutralDarkGrey = Color(0xFF887D75)
 val NeutralBlackGrey = Color(0xFF262626)
 val NeutralBlack = Color(0xFF000000)
-
-val TutorialOnlyGreen = Color(0xFF00CD83)
-val TutorialOnlyMagenta = Color(0xFFEE4481)
-val TutorialOnlyOrange = Color(0xFFEF811C)
-val TutorialOnlyBlue = Color(0xFF025FBF)
