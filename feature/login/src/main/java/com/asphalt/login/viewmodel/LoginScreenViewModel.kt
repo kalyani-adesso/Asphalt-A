a980a49ab64dd758630290e771cc11303b59dc05
Harikumar <harikumar.sivasankaran@adesso.in>
2025-09-26 16:08:37 +0530

    Email validation aded

--- feature/login/src/main/java/com/asphalt/login/viewmodel/LoginScreenViewModel.kt
+++ feature/login/src/main/java/com/asphalt/login/viewmodel/LoginScreenViewModel.kt
@@ -1,5 +1,7 @@
 package com.asphalt.login.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.asphalt.commonui.util.EmailValidator
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -7,13 +9,23 @@ import kotlinx.coroutines.flow.StateFlow
 
 class LoginScreenViewModel : ViewModel() {
     private val _emailTextMutableState = MutableStateFlow("")
-    val _emailTextState: StateFlow<String> = _emailTextMutableState
-    val _isEmailVaild = MutableStateFlow(false)
+
+    val emailTextState: StateFlow<String> = _emailTextMutableState
+
+    private val _passwordTextMutableState = MutableStateFlow("")
+    val passwordTextState = _passwordTextMutableState
+    val isEmailVaild = MutableStateFlow(false)
 
 
     fun updateEmailState(email: String) {
         _emailTextMutableState.value = email
-        _isEmailVaild.value = EmailValidator.isValid(email)
+        isEmailVaild.value = EmailValidator.isValid(email)
     }
+
+    fun updatePassword(password: String) {
+        _passwordTextMutableState.value = password
+
+    }
+
 
 }
