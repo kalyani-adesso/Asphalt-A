8be4cf00bd0d6a7e7f03a26c18eb6cf1735ccec8
Adarsha Hebbar <adarsha.hebbar.ext@adevinta.com>
2025-09-29 14:45:51 +0530

    [MBR - 0006] : Login text field logic changes and login success design.

---
+++ iosApp/iosApp/LoginSucessView/LoginSucessView.swift
@@ -0,0 +1,50 @@
+//
+//  LoginSucessView.swift
+//  iosApp
+//
+//  Created by Adarsha Hebbar on 29/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct LoginSucessView: View {
+    var body: some View {
+        VStack {
+            AppImage.Login.loginSuccessBg
+                .padding(.bottom,82)
+                .frame(height: 278)
+            Text(AppStrings.SignInSucessView.loginSuccessTitle.rawValue)
+                .font(KlavikaFont.bold.font(size: 24))
+                .padding(.bottom,16)
+                .foregroundStyle(AppColor.black)
+            Text(AppStrings.SignInSucessView.loginSuccessSubtitle.rawValue)
+                .font(KlavikaFont.medium.font(size: 16))
+                .foregroundStyle(AppColor.stoneGray)
+                .padding(.bottom,82)
+                .frame(alignment: .center)
+            Text(AppStrings.SignInLabel.signInTitle.rawValue)
+                .frame(maxWidth: .infinity)
+                .frame(height: 60)
+                .background(
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            AppColor.royalBlue,
+                            AppColor.pursianBlue,
+                        ]),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                )
+                .cornerRadius(15)
+                .shadow(color: Color.black.opacity(0.20), radius: 4, x: 0, y: 2)
+                .foregroundStyle(AppColor.white)
+                .font(KlavikaFont.bold.font(size: 18))
+                .padding(.bottom, 21)
+        }
+        .padding([.leading, .trailing], 46)
+        .padding(.top, 100)
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
