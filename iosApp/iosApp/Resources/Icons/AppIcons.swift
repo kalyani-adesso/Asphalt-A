656cd3faee0e858738d15a5b56eb80710cf1c64d
Adarsha Hebbar <adarsha.hebbar.ext@adevinta.com>
2025-09-29 10:14:48 +0530

    [MBR - 0005] : Login Feature.

---
+++ iosApp/iosApp/Resources/Icons/AppIcons.swift
@@ -0,0 +1,32 @@
+//
+//  Incons.swift
+//  iosApp
+//
+//  Created by Adarsha Hebbar on 29/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppIcon {
+    struct Login {
+        static let email = Image("email")
+        static let eyeSlash = Image("eye-slash")
+        static let loginRemembered = Image("login-rememberd")
+        static let passwordEncrypt = Image("password-encrypt")
+        static let password = Image("password")
+        static let rememberLogin = Image("remember-login")
+        static let validEmail = Image("vaild-Email")
+    }
+    
+    struct SignUp {
+        static let smsEdit = Image("sms-edit")
+        static let userDetail = Image("user-detail")
+    }
+    
+    struct Welcome {
+        static let arrow = Image("arrow")
+    }
+}
+
+
