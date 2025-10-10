0352c4ded8b607acd55176284feb63564f8ee1d7
sreedevpr <prsreedev1996@gmail.com>
2025-10-10 15:11:26 +0530

    minor changes

--- feature/dashboard/src/main/java/com/asphalt/dashboard/composables/components/PlacesVisitedGraph.kt
+++ feature/dashboard/src/main/java/com/asphalt/dashboard/composables/components/PlacesVisitedGraph.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.asphalt.commonui.R
 import com.asphalt.commonui.constants.Constants
 import com.asphalt.commonui.theme.Dimensions
@@ -54,7 +53,11 @@ fun PlacesVisitedGraph() {
                 )
 
                 Text(
-                    "${formatter.format(sevenMonthsAgo)} - ${formatter.format(currentDate)}",
+                    "${formatter.format(sevenMonthsAgo).uppercase()} - ${
+                        formatter.format(
+                            currentDate
+                        ).uppercase()
+                    }",
                     color = NeutralGrey80,
                     style = TypographyBold.headlineLarge,
                     fontSize = Dimensions.textSize12
