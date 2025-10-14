f38a59975801233c74611aa2c4029e6067cb423b
Harikumar <harikumar.sivasankaran@adesso.in>
2025-10-14 14:28:28 +0530

    tab ui implementation

--- feature/createride/src/main/java/com/asphalt/createride/ui/CreateRideScreen.kt
+++ feature/createride/src/main/java/com/asphalt/createride/ui/CreateRideScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -16,30 +18,29 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asphalt.commonui.R
 import com.asphalt.commonui.constants.Constants
 import com.asphalt.commonui.theme.AsphaltTheme
 import com.asphalt.commonui.theme.Dimensions
-import com.asphalt.commonui.theme.NaturalGreen
 import com.asphalt.commonui.theme.NeutralBlack
 import com.asphalt.commonui.theme.NeutralDarkGrey
 import com.asphalt.commonui.theme.NeutralWhite
 import com.asphalt.commonui.theme.PrimaryDarkerLightB75
-import com.asphalt.commonui.theme.TypographyBold
+import com.asphalt.commonui.theme.PrimaryDeepBlue
 import com.asphalt.commonui.theme.Typography
+import com.asphalt.commonui.theme.TypographyBold
 import com.asphalt.commonui.ui.ActionBarWithBack
+import com.asphalt.commonui.ui.BorderedButton
+import com.asphalt.commonui.ui.GradientButton
+import com.asphalt.commonui.utils.ComposeUtils
 import com.asphalt.createride.viewmodel.CreateRideScreenViewModel
 
 @Composable
@@ -58,36 +59,94 @@ fun CreateRideEntry(viewModel: CreateRideScreenViewModel = viewModel()) {
                 ActionBarWithBack(R.drawable.ic_arrow_back, "Create a Ride") { }
                 TabSelection(viewModel)
             }
-
+            BottomButtons(viewModel)
             // Fixed bottom button
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .background(color = NeutralWhite)
-                    .padding(
-                        bottom = WindowInsets.navigationBars.asPaddingValues()
-                            .calculateBottomPadding()
-                    )
-            ) {
-                Button(onClick = {
-                    if (viewModel.tabSelectState.value > 1)
-                        viewModel.updateTab(-1)
-                    // Button action here
-                }) {
-                    Text(text = "-", color = Color.White)
-                }
-                Button(onClick = {
-                    if (viewModel.tabSelectState.value < 5)
-                        viewModel.updateTab(1)
-                    // Button action here
-                }, modifier = Modifier.padding(start = 120.dp)) {
-                    Text(text = "+", color = Color.White)
-                }
-            }
+
         }
     }
 }
 
+@Composable
+fun BoxScope.BottomButtons(viewModel: CreateRideScreenViewModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.BottomCenter)
+            .background(color = NeutralWhite)
+            .padding(
+                bottom = WindowInsets.navigationBars.asPaddingValues()
+                    .calculateBottomPadding()+ Dimensions.spacing20,
+                start = Dimensions.padding16,
+                end = Dimensions.padding16
+            )
+    ) {
+        if (viewModel.tabSelectState.value == Constants.TAB_DETAILS) {//viewModel.tabSelectState.value == Constants.TAB_DETAILS
+
+            GradientButton(
+                onClick = {
+                    viewModel.updateTab(1)
+                },
+            ) {
+                ComposeUtils.DefaultButtonContent("Next Step".uppercase())
+            }
+        } else {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(
+                    Dimensions.spacing20
+                )
+            ) {
+                BorderedButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {
+                        if (viewModel.tabSelectState.value > 1) {
+                            viewModel.updateTab(-1)
+                        }
+                    },
+                    contentPaddingValues = PaddingValues(
+                        Dimensions.size0
+                    )
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            "Previous".uppercase(),
+                            color = PrimaryDarkerLightB75,
+                            style = TypographyBold.bodyMedium
+                        )
+                    }
+                }
+
+                GradientButton(
+                    modifier = Modifier.weight(1f),
+                    endColor = PrimaryDeepBlue,
+                    onClick = {
+                        if (viewModel.tabSelectState.value < 5) {
+                            viewModel.updateTab(1)
+                        }
+                    },
+                    contentPadding = PaddingValues(
+                        Dimensions.size0
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "Next".uppercase(),
+                            color = NeutralWhite,
+                            style = TypographyBold.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+}
 @Composable
 fun TabSelection(viewModel: CreateRideScreenViewModel) {
     Spacer(Modifier.height(Dimensions.size30))
@@ -134,16 +193,17 @@ fun TabSelection(viewModel: CreateRideScreenViewModel) {
             Spacer(modifier = Modifier.height(Dimensions.size10))
             Text(
                 text = "Details",
-                style = if(viewModel.tabSelectState.value== Constants.TAB_DETAILS){
+                style = if (viewModel.tabSelectState.value == Constants.TAB_DETAILS) {
                     TypographyBold.bodySmall
-                }else{
+                } else {
                     Typography.bodySmall
                 },
-                color = if(viewModel.tabSelectState.value== Constants.TAB_DETAILS){
+                color = if (viewModel.tabSelectState.value == Constants.TAB_DETAILS) {
                     NeutralBlack
-                }else{
+                } else {
                     NeutralDarkGrey
-                }, fontSize = Dimensions.textSize12,
+                },
+                fontSize = Dimensions.textSize12,
             )
         }
         Column(
@@ -183,14 +243,14 @@ fun TabSelection(viewModel: CreateRideScreenViewModel) {
             Spacer(modifier = Modifier.height(Dimensions.size10))
             Text(
                 text = "Route",
-                style = if(viewModel.tabSelectState.value== Constants.TAB_ROUTE){
+                style = if (viewModel.tabSelectState.value == Constants.TAB_ROUTE) {
                     TypographyBold.bodySmall
-                }else{
+                } else {
                     Typography.bodySmall
                 },
-                color = if(viewModel.tabSelectState.value== Constants.TAB_ROUTE){
+                color = if (viewModel.tabSelectState.value == Constants.TAB_ROUTE) {
                     NeutralBlack
-                }else{
+                } else {
                     NeutralDarkGrey
                 }, fontSize = Dimensions.textSize12
             )
@@ -229,14 +289,14 @@ fun TabSelection(viewModel: CreateRideScreenViewModel) {
             Spacer(modifier = Modifier.height(Dimensions.size10))
             Text(
                 text = "Participants",
-                style = if(viewModel.tabSelectState.value== Constants.TAB_PARTICIPANT){
+                style = if (viewModel.tabSelectState.value == Constants.TAB_PARTICIPANT) {
                     TypographyBold.bodySmall
-                }else{
+                } else {
                     Typography.bodySmall
                 },
-                color = if(viewModel.tabSelectState.value== Constants.TAB_PARTICIPANT){
+                color = if (viewModel.tabSelectState.value == Constants.TAB_PARTICIPANT) {
                     NeutralBlack
-                }else{
+                } else {
                     NeutralDarkGrey
                 }, maxLines = 1, fontSize = Dimensions.textSize12
             )
@@ -275,14 +335,14 @@ fun TabSelection(viewModel: CreateRideScreenViewModel) {
             Spacer(modifier = Modifier.height(Dimensions.size10))
             Text(
                 text = "Review",
-                style = if(viewModel.tabSelectState.value== Constants.TAB_REVIEW){
+                style = if (viewModel.tabSelectState.value == Constants.TAB_REVIEW) {
                     TypographyBold.bodySmall
-                }else{
+                } else {
                     Typography.bodySmall
                 },
-                color = if(viewModel.tabSelectState.value== Constants.TAB_REVIEW){
+                color = if (viewModel.tabSelectState.value == Constants.TAB_REVIEW) {
                     NeutralBlack
-                }else{
+                } else {
                     NeutralDarkGrey
                 }, fontSize = Dimensions.textSize12
             )
@@ -321,14 +381,14 @@ fun TabSelection(viewModel: CreateRideScreenViewModel) {
             Spacer(modifier = Modifier.height(Dimensions.size10))
             Text(
                 text = "Share",
-                style = if(viewModel.tabSelectState.value== Constants.TAB_SHARE){
+                style = if (viewModel.tabSelectState.value == Constants.TAB_SHARE) {
                     TypographyBold.bodySmall
-                }else{
+                } else {
                     Typography.bodySmall
                 },
-                color = if(viewModel.tabSelectState.value== Constants.TAB_SHARE){
+                color = if (viewModel.tabSelectState.value == Constants.TAB_SHARE) {
                     NeutralBlack
-                }else{
+                } else {
                     NeutralDarkGrey
                 }, fontSize = Dimensions.textSize12
             )
