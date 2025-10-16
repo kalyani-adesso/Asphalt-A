0f3011bd86a89529ed4a3167415d38e97365d30c
Harikumar <harikumar.sivasankaran@adesso.in>
2025-10-16 14:19:32 +0530

    date picker dialog implentation

--- feature/createride/src/main/java/com/asphalt/createride/ui/composables/DetailsSection.kt
+++ feature/createride/src/main/java/com/asphalt/createride/ui/composables/DetailsSection.kt
@@ -15,11 +15,16 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,17 +38,22 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asphalt.commonui.R
+import com.asphalt.commonui.theme.BlueLight
 import com.asphalt.commonui.theme.Dimensions
 import com.asphalt.commonui.theme.NeutralBlack
 import com.asphalt.commonui.theme.NeutralDarkGrey
 import com.asphalt.commonui.theme.NeutralLightPaper
 import com.asphalt.commonui.theme.NeutralWhite
+import com.asphalt.commonui.theme.PrimaryDarkerLightB75
 import com.asphalt.commonui.theme.Typography
 import com.asphalt.commonui.theme.TypographyMedium
 import com.asphalt.createride.viewmodel.CreateRideScreenViewModel
+import java.time.LocalDate
+import java.util.Calendar
 
 @Composable
 fun DetailsSection(viewModel: CreateRideScreenViewModel) {
@@ -53,6 +63,7 @@ fun DetailsSection(viewModel: CreateRideScreenViewModel) {
     var expanded by remember { mutableStateOf(false) }
     var anchorWidth by remember { mutableStateOf(0) }
     val rideType = viewModel.getRideType(context)
+    DatePickerSample()
 
     Column(
         modifier = Modifier
@@ -320,6 +331,77 @@ fun DetailsSection(viewModel: CreateRideScreenViewModel) {
         Spacer(modifier = Modifier.height(Dimensions.padding16))
     }
 }
+
+
+@Composable
+fun DatePickerSample() {
+    //val dateFormatter = remember { SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()) }
+    var pickedDate by remember { mutableStateOf<LocalDate?>(null) }
+    var showDatePicker by remember { mutableStateOf(true) }
+    if (showDatePicker) {
+        val calendar = remember {
+            Calendar.getInstance().apply {
+                // Set to start of day (optional)
+                //set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }
+        }
+        val currentDateMillis = calendar.timeInMillis
+        val datePickerState = rememberDatePickerState(initialSelectedDateMillis = currentDateMillis)
+        DatePickerDialog(
+            onDismissRequest = { showDatePicker = false },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        val selectedDateMillis = datePickerState.selectedDateMillis
+                        val selectedDateSeconds = selectedDateMillis?.let { it / 1000L }
+//                        selectedDateMillis?.let {
+//                            pickedDate = Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault())
+//                                .toLocalDate()
+//                        }
+                        showDatePicker = false
+                    }
+                ) {
+                    Text(
+                        text = stringResource(R.string.ok),
+                        style = Typography.bodyMedium,
+                        color = BlueLight
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDatePicker = false }) {
+                    Text(
+                        text = stringResource(R.string.cancel),
+                        style = Typography.bodyMedium,
+                        color = BlueLight
+                    )
+                }
+            }
+        ) {
+            DatePicker(
+                state = datePickerState,
+                showModeToggle = false,
+                title = {
+                    Text(
+                        text = stringResource(R.string.select_date), style = Typography.bodySmall,
+                        modifier = Modifier.padding(
+                            start = Dimensions.padding16,
+                            top = Dimensions.padding16
+                        )
+                    )
+                },
+                colors = DatePickerDefaults.colors(
+                    headlineContentColor = NeutralBlack,
+                    selectedDayContainerColor = PrimaryDarkerLightB75
+                )
+            )
+        }
+    }
+}
+
 
 @Preview
 @Composable
