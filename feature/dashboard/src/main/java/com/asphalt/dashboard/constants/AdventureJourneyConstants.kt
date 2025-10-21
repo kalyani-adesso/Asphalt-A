5cfbbd5aa71bb975f19e1ddc2ebf4afb3e02c79c
sreedevpr <prsreedev1996@gmail.com>
2025-10-22 04:12:01 +0530

    add new choices in adventure journey

--- feature/dashboard/src/main/java/com/asphalt/dashboard/constants/AdventureJourneyConstants.kt
+++ feature/dashboard/src/main/java/com/asphalt/dashboard/constants/AdventureJourneyConstants.kt
@@ -1,7 +1,9 @@
 package com.asphalt.dashboard.constants
 
 object AdventureJourneyConstants {
-    const val CHOICE_LAST_FOUR_MONTHS = 1
-    const val CHOICE_LAST_WEEK = 2
-    const val CHOICE_LAST_YEAR = 3
+    const val CHOICE_LAST_FOUR_MONTHS = 3
+    const val CHOICE_THIS_MONTH = 1
+    const val CHOICE_LAST_YEAR = 5
+    const val CHOICE_LAST_SIX_MONTHS = 4
+    const val CHOICE_LAST_MONTH = 2
 }
--- feature/dashboard/src/main/java/com/asphalt/dashboard/sealedclasses/AdventureJourneyTimeFrameChoices.kt
+++ feature/dashboard/src/main/java/com/asphalt/dashboard/sealedclasses/AdventureJourneyTimeFrameChoices.kt
@@ -9,14 +9,34 @@ sealed class AdventureJourneyTimeFrameChoices(val choiceRes: Int, val choiceId: 
         AdventureJourneyConstants.CHOICE_LAST_FOUR_MONTHS
     )
 
-    object LastWeek : AdventureJourneyTimeFrameChoices(
-        R.string.last_week,
-        AdventureJourneyConstants.CHOICE_LAST_WEEK
+    object LastSixMonths : AdventureJourneyTimeFrameChoices(
+        R.string.last_six_months,
+        AdventureJourneyConstants.CHOICE_LAST_SIX_MONTHS
+    )
+
+    object ThisMonth : AdventureJourneyTimeFrameChoices(
+        R.string.this_month,
+        AdventureJourneyConstants.CHOICE_THIS_MONTH
     )
 
     object LastYear : AdventureJourneyTimeFrameChoices(
         R.string.last_year,
         AdventureJourneyConstants.CHOICE_LAST_YEAR
     )
+
+    object LastMonth : AdventureJourneyTimeFrameChoices(
+        R.string.last_month,
+        AdventureJourneyConstants.CHOICE_LAST_MONTH
+    )
+
+    companion object {
+        fun getAllChoices(): List<AdventureJourneyTimeFrameChoices> {
+            return listOf(
+                LastMonth, ThisMonth, LastYear, LastFourMonths, LastSixMonths
+            ).sortedBy {
+                it.choiceId
+            }
+        }
+    }
 
 }
