974a8787c5bd12f1ad2ad55da97275c7dbdadc41
Harikumar <harikumar.sivasankaran@adesso.in>
2025-10-24 14:17:55 +0530

    UI fixed

--- feature/resetpassword/src/main/java/com/asphalt/resetpassword/screens/CreatePasswordScreen.kt
+++ feature/resetpassword/src/main/java/com/asphalt/resetpassword/screens/CreatePasswordScreen.kt
@@ -3,6 +3,7 @@ package com.asphalt.resetpassword.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,12 +25,18 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -64,6 +71,8 @@ fun CreatePasswordScreen(
     val state = viewModel.createPasswordState.collectAsState()
     viewModel.updateEmail(email)
     val scrollState = rememberScrollState()
+    var showPassword by remember { mutableStateOf(false) }
+    var showConfirmPassword by remember { mutableStateOf(false) }
     AsphaltTheme {
         Scaffold { paddingValues ->
 
@@ -231,7 +240,13 @@ fun CreatePasswordScreen(
                             },
                             textStyle = Typography.bodySmall,
                             singleLine = true,
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                            visualTransformation = if (showPassword) {
+                                VisualTransformation.None
+                            } else {
+                                PasswordVisualTransformation()
+
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                             modifier = Modifier.fillMaxWidth(),
                             colors = TextFieldDefaults.colors(
                                 unfocusedContainerColor = Color.Transparent,
@@ -254,9 +269,12 @@ fun CreatePasswordScreen(
                             trailingIcon = {
 
                                 Icon(
-                                    painter = painterResource(R.drawable.ic_eye_slash),
+                                    painter = painterResource(if (showPassword) R.drawable.ic_eye_slash else R.drawable.ic_eye),
                                     contentDescription = "Email icon",
-                                    tint = Color.Unspecified
+                                    tint = Color.Unspecified,
+                                    modifier = Modifier.clickable {
+                                        showPassword = !showPassword
+                                    }
 
                                 )
 
@@ -307,7 +325,13 @@ fun CreatePasswordScreen(
                             },
                             textStyle = Typography.bodySmall,
                             singleLine = true,
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                            visualTransformation = if (showConfirmPassword) {
+                                VisualTransformation.None
+                            } else {
+                                PasswordVisualTransformation()
+
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                             modifier = Modifier.fillMaxWidth(),
                             colors = TextFieldDefaults.colors(
                                 unfocusedContainerColor = Color.Transparent,
@@ -330,9 +354,12 @@ fun CreatePasswordScreen(
                             trailingIcon = {
 
                                 Icon(
-                                    painter = painterResource(R.drawable.ic_eye_slash),
+                                    painter = painterResource(if (showConfirmPassword) R.drawable.ic_eye_slash else R.drawable.ic_eye),
                                     contentDescription = "Email icon",
-                                    tint = Color.Unspecified
+                                    tint = Color.Unspecified,
+                                    modifier = Modifier.clickable {
+                                        showConfirmPassword = !showConfirmPassword
+                                    }
 
                                 )
 
@@ -363,8 +390,8 @@ fun CreatePasswordScreen(
                     GradientButton(
                         startColor = PrimaryDarkerLightB75,
                         endColor = PrimaryDarkerLightB50, onClick = {
-                            if(viewModel.createPassword())
-                            onUpdateClick.invoke()
+                            if (viewModel.createPassword())
+                                onUpdateClick.invoke()
                         }
                     ) {
                         ComposeUtils.DefaultButtonContent(
