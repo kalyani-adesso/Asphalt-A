2eb3fa3d03b42bbffe022ff02c2d350beb7c9900
Adarsha Hebbar <adarsha.hebbar.ext@adevinta.com>
2025-10-28 17:55:06 +0530

    [MBR- 036] : Profile model creation

---
+++ shared/src/commonMain/kotlin/com/asphalt/android/model/ProfileResult.kt
@@ -0,0 +1,25 @@
+package com.asphalt.android.model
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProfileInfo(
+    val userName: String,
+    val email: String,
+    val phoneNumber: String,
+    val emergencyContact: String,
+    val drivingLicense: String,
+    val isMechanic: Boolean
+)
+
+@Serializable
+data class BikeInfo(
+    val bikeType: String,
+    val make: String,
+    val model: String
+)
+
+@Serializable
+data class UserProfile(
+    val profile: ProfileInfo? = null,
+    val bikeInfo: Map<String, BikeInfo>? = null
+)
