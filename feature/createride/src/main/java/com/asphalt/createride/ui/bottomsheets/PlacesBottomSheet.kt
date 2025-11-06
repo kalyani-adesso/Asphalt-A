6584d39da557d682f724d476d9ac235b0943af5c
Harikumar <harikumar.sivasankaran@adesso.in>
2025-11-06 18:06:10 +0530

    ui completed

--- feature/createride/src/main/java/com/asphalt/createride/ui/bottomsheets/PlacesBottomSheet.kt
+++ feature/createride/src/main/java/com/asphalt/createride/ui/bottomsheets/PlacesBottomSheet.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
@@ -30,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -115,8 +118,14 @@ fun BottomSheetLayout(
                 singleLine = true,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(),
-
+                    .fillMaxHeight(), keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search // Shows a "Search" icon on keyboard
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        placesVM?.getPlaces(text) // Trigger your search operation
+                    }
+                ),
                 colors = TextFieldDefaults.colors(
                     unfocusedContainerColor = Color.Transparent,
                     focusedContainerColor = Color.Transparent,
