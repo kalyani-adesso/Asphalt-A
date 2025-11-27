1ca179cd5bda289eaa20571c2ba03640a55e082b
Adarsha Hebbar <adarsha.hebbar.ext@adevinta.com>
2025-11-27 16:09:53 +0530

    [MBR-066]: String localization

--- iosApp/iosApp/Resources/Constants/AppStrings.swift
+++ iosApp/iosApp/Resources/Constants/AppStrings.swift
@@ -378,7 +378,12 @@ public struct AppStrings{
         static let joinaRideTitle = "Join a Ride"
         static let reJoinRideTitle = "ReJoin Ride"
         static let rideActive = "Ride Started! Navigation active."
-        
+        static let noActiveRidesFound = "No active rides found"
+        static let confirmEndCurrentRide = "Do you want to end your current ride and join this one?"
+        static let rideAlreadyActive = "Ride already active"
+        static let loading = "Loading..."
+        static let yes = "Yes"
+        static let no = "No"
     }
     
     enum ConnectedRide {
