3316bdd4f76ef55259c0bb14f35fe07807e8edcd
Harikumar <harikumar.sivasankaran@adesso.in>
2025-12-09 17:30:46 +0530

    UI implemnetation

--- feature/chat/src/main/java/com/asphalt/chat/screen/ChatPopUp.kt
+++ feature/chat/src/main/java/com/asphalt/chat/screen/ChatPopUp.kt
@@ -7,31 +7,45 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.asphalt.commonui.R
+import com.asphalt.commonui.R.string
 import com.asphalt.commonui.theme.Dimensions
+import com.asphalt.commonui.theme.NeutralDarkGrey
 import com.asphalt.commonui.theme.NeutralWhite
 import com.asphalt.commonui.theme.PrimaryDarkerLightB75
 import com.asphalt.commonui.theme.PrimaryLight
+import com.asphalt.commonui.theme.Typography
 import com.asphalt.commonui.ui.CircularNetworkImage
 import com.asphalt.commonui.ui.RoundedBox
 
@@ -39,7 +53,11 @@ import com.asphalt.commonui.ui.RoundedBox
 fun ChatDialog(
     onDismiss: () -> Unit
 ) {
-    Dialog(onDismissRequest = onDismiss,  properties = DialogProperties(usePlatformDefaultWidth = false)) {
+    var text by remember { mutableStateOf("") }
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
         Column(modifier = Modifier.padding(horizontal = 20.dp)) {
             Box(
                 modifier = Modifier
@@ -63,7 +81,8 @@ fun ChatDialog(
                     Column(
                         modifier = Modifier
                             .height(72.dp)
-                            .fillMaxWidth().background(
+                            .fillMaxWidth()
+                            .background(
                                 PrimaryLight, RoundedCornerShape(
                                     20.dp, 20.dp, 0.dp, 0.dp
                                 )
@@ -105,14 +124,62 @@ fun ChatDialog(
                         }
 
                     }
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(start = Dimensions.size10, end = Dimensions.size10)
-                    ) {
-                        items(messages) { msg ->
-                            ChatBubble(msg)
+                    Box(modifier = Modifier.weight(1f)) {
+
+
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(start = Dimensions.size10, end = Dimensions.size10)
+                        ) {
+                            items(messages) { msg ->
+                                ChatBubble(msg)
+                            }
                         }
                     }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                            .padding(start = 10.dp, end = 10.dp)
+                            .background(
+                                NeutralWhite, shape = RoundedCornerShape(Dimensions.padding10)
+                            )
+                            .border(
+                                width = Dimensions.padding1,
+                                color = PrimaryDarkerLightB75,
+                                shape = RoundedCornerShape(Dimensions.padding10)
+                            ), verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextField(
+                            value = text,
+                            onValueChange = { text = it },
+                            placeholder = {
+                                Text(
+                                    text = stringResource(string.type_msg),
+                                    style = Typography.bodySmall,
+                                    color = NeutralDarkGrey
+                                )
+                            },
+                            textStyle = Typography.bodySmall,
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = TextFieldDefaults.colors(
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent,
+                                disabledContainerColor = Color.Transparent,
+
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                errorIndicatorColor = Color.Transparent
+                            ),
+
+                            )
+
+                    }
+                    Spacer(Modifier.height(10.dp))
                 }
             }
         }
