45298cf8ca5a8e8e5c4903b1d9295c9f36dbd7c6
Harikumar <harikumar.sivasankaran@adesso.in>
2026-04-06 12:31:30 +0530

    Product Details screen

---
+++ feature/marketplace/src/main/java/com/asphalt/marketplace/ui/ProductDetails.kt
@@ -0,0 +1,15 @@
+package com.asphalt.marketplace.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProductDetailsScreen() {
+
+}
+
+@Composable
+@Preview
+fun ProductDetailsPreview() {
+    ProductDetailsScreen()
+}
